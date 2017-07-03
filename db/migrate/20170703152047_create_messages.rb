4ddf1f6bbe46628f012e590dc6b3a9d3c926f7cd
Braidy Merkle & Zach Parsons <pair+bmerkle3.zachparsons@devbootcamp.com>
2017-07-03 10:35:57 -0500

    add user phone to schema & user migration & model

---
+++ db/migrate/20170703152047_create_messages.rb
@@ -0,0 +1,14 @@
+class CreateMessages < ActiveRecord::Migration[5.1]
+  def change
+    create_table :messages do |t|
+      t.string :caption
+      t.string :image_url
+      t.integer :sender_id
+      t.integer :receiver_id
+      t.datetime :deliver_at
+      t.boolean :deliverable
+
+      t.timestamps
+    end
+  end
+end
