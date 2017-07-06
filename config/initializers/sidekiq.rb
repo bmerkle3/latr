e37ed2a158c9eba7c41558e4ddb58dffd1b6209f
Braidy Merkle & Zach Parsons <pair+bmerkle3.zachparsons@devbootcamp.com>
2017-07-06 16:33:27 -0500

    changes the client size from 1 to 2 to see if that changes the pool size and in what way

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 # three unicorns = 3 connections
 Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
+  config.redis = { :size => 2 }
 end
 # so one sidekiq can have 7 connections
 Sidekiq.configure_server do |config|
