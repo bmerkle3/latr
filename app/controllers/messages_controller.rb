fdf392e2d604ebecec06ca13b51ccb020dbe4222
Braidy Merkle & Zach Parsons <pair+bmerkle3.zachparsons@devbootcamp.com>
2017-07-07 09:57:47 -0500

    removed p statements

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,12 +11,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-    p message_params
-    # .strftime("%-m/%-d/%y")
-
     # from_time = Time.now
-    p "*"*80
-    p original_time = @message.deliver_at.to_s
     to_time = DateTime.parse(@message.deliver_at.to_s)
     # @send_in = helpers.distance_of_time_in_words(from_time, to_time)
     # @message.deliver_at = to_time
--- app/workers/message_worker.rb
+++ app/workers/message_worker.rb
@@ -4,7 +4,6 @@ class MessageWorker
 
   def perform(message_id)
     @message = Message.find(message_id)
-    p "*" * 90
     puts "SIDEKIQ WORKER SENDING A MESSAGE TO #{@message.receiver.phone}"
     @message.send_sms(@message.clean_number, @message)
   end
