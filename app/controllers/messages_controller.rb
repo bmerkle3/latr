74366d5c1b2712aeb17dccba3b9376ea90094ae3
Braidy Merkle & Zach Parsons <pair+bmerkle3.zachparsons@devbootcamp.com>
2017-07-07 11:15:28 -0500

    add delay delivery of message logic by UTC days

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,16 +10,17 @@ class MessagesController < ApplicationController
   end
 
   def create
-    p @message = Message.new(message_params)
+    @message = Message.new(message_params)
 
-    # p from_time = DateTime.now
-    # p to_time = @message.deliver_at
-    # @send_in = helpers.distance_of_time_in_words(from_time, to_time)
-    # @message.deliver_at = to_time
+    from_time = DateTime.now.utc
+    to_time = @message.deliver_at
+    dif_time = to_time - from_time
+    dif_days = dif_time / 60 / 60 / 24
+    dif_days
 
     if @message.save
-      # MessageWorker.perform_in(5.minutes, @message.id)
-      MessageWorker.perform_async(@message.id)
+      MessageWorker.perform_in(dif_days.days, @message.id)
+      # MessageWorker.perform_async(@message.id)
       redirect_to root_path
     else
       redirect_to messages_new_path
@@ -33,7 +34,6 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    # remove .require(:message) for deploy to heroku
     params.permit(:caption, :image, :sender_id, :receiver_id, :deliver_at, :deliverable)
   end
 end
